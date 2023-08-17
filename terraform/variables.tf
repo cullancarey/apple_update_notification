189b57e5fb62e496c1022c158feb566777dde357
Cullan Carey <cullancarey@MacBook-Pro-3.local>
2023-08-17 15:21:09 -0400

    updating

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -13,3 +13,8 @@ variable "environment" {
   type        = string
   description = "The environment of the Apple update notification project."
 }
+
+variable "twitter_username" {
+  type        = string
+  description = "The username of the twitter account to use."
+}
