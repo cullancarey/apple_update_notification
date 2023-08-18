31195edc69efa61685592790276516268c8bac74
Cullan Carey <cullancarey@MacBook-Pro-3.local>
2023-08-18 17:49:18 -0400

    using lambda layer

--- terraform/lambda_layer.tf
+++ terraform/lambda_layer.tf
@@ -1,5 +1,5 @@
 resource "aws_lambda_layer_version" "lambda_utils_layer" {
-  filename   = "apple_utils.zip"
+  filename   = "lambda_build/apple_utils.zip"
   layer_name = "apple_utils"
 
   compatible_runtimes = ["python3.9"]
