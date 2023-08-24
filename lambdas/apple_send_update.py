0494c5792af7aa57c7fbc404f45bd2c2ba5b6d3d
Cullan Carey <cullancarey@MacBook-Pro-3.local>
2023-08-24 13:52:12 -0400

    adding send update lambda

--- lambdas/apple_send_update.py
+++ lambdas/apple_send_update.py
@@ -20,4 +20,16 @@ logger.setLevel(logging.INFO)
 def lambda_handler(event, context):
     """Main function for lambda function"""
 
-    logger.info(event)
+    for record in event['Records']:
+
+        device = record['dynamodb']['Keys']['device']['S']
+        release_statement = record['dynamodb']['NewImage']['ReleaseStatement']['S']
+        release_version = record['dynamodb']['NewImage']['ReleaseVersion']['S']
+        time_created = record['dynamodb']['ApproximateCreationDateTime']
+        event_name = record['eventName']
+        event_version = record['eventVersion']
+
+        stream_details = {"device": device, "release_version": release_version, "time_created": time_created, "event_name": event_name, "event_version": event_version}
+
+        logger.info(stream_details)
+
