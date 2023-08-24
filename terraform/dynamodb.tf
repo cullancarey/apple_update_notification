d4568d5abc46eaec09cf4269c0e5cc40b794d3cc
Cullan Carey <cullancarey@MacBook-Pro-3.local>
2023-08-24 12:21:11 -0400

    adding send update lambda

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -1,13 +1,13 @@
 resource "aws_dynamodb_table" "apple_os_updates_table" {
   name                        = "apple_os_updates_${var.environment}"
   billing_mode                = "PAY_PER_REQUEST"
-  hash_key                    = "timestamp"
-  deletion_protection_enabled = true
-  stream_enabled              = true
+  hash_key                    = "device"
+  deletion_protection_enabled = false
+  stream_enabled              = false
   stream_view_type            = "NEW_IMAGE"
 
   attribute {
-    name = "timestamp"
-    type = "N"
+    name = "device"
+    type = "S"
   }
 }
